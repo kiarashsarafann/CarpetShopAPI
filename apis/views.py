46cb1e2e86e62d62b34480499d9b0b3511139afa
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2024-07-25 09:46:03 +0330

    create user and carpet viewsets

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+
+
+class CarpetViewSet(viewsets.ModelViewSet):
+    queryset = Carpet.objects.all()
+    serializer_class = CarpetSerializers
