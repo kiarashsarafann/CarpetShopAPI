8dcaa03a2bc9441cc7749d4c018e1652bc532f1d
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2024-08-17 15:27:20 +0330

    add admin

--- carpets/admin.py
+++ carpets/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Carpet
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Carpet)
+admin.site.register(Material)
+admin.site.register(Size)
+admin.site.register(Reed)
