976834d1c6357ac8cee8be88a8f80ec331e64dd8
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2024-08-17 15:27:40 +0330

    add models

--- carpets/models.py
+++ carpets/models.py
@@ -1,7 +1,34 @@
 from django.db import models
 
 
-# Create your models here.
-
 class Carpet(models.Model):
-    title = models.CharField(max_length=128, verbose_name='نام فرش')
+    title = models.CharField(max_length=64, verbose_name='نام فرش')
+    image = models.ImageField(upload_to='uploads', verbose_name="عکس")
+    size = models.ForeignKey(to='Size', on_delete=models.CASCADE, related_name='سایز', verbose_name="سایز فرش")
+    material = models.ForeignKey(to='Material', on_delete=models.CASCADE, related_name='جنس', verbose_name='جنس فرش')
+    reed = models.ForeignKey(to='Reed', on_delete=models.CASCADE, related_name='شانه', verbose_name='شانه قرش')
+
+    def __str__(self):
+        return self.title
+
+
+class Size(models.Model):
+    title = models.CharField(max_length=16, verbose_name="سایز")
+
+    def __str__(self):
+        return self.title
+
+
+class Material(models.Model):
+    title = models.CharField(max_length=64, verbose_name='جنس')
+    image = models.ImageField(upload_to='uploads', verbose_name="عکس")
+
+    def __str__(self):
+        return self.title
+
+
+class Reed(models.Model):
+    title = models.CharField(max_length=64, verbose_name='نام شانه')
+
+    def __str__(self):
+        return self.title
