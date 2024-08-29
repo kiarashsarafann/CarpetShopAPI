a9952be4c8d64f4549c134e1451b99e4a00f033d
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2024-08-29 19:20:28 +0330

    add price to carpet and material

--- carpets/models.py
+++ carpets/models.py
@@ -8,6 +8,7 @@ class Carpet(models.Model):
     material = models.ForeignKey(to='Material', on_delete=models.CASCADE, related_name='جنس', verbose_name='جنس فرش')
     reed = models.ForeignKey(to='Reed', on_delete=models.CASCADE, related_name='شانه', verbose_name='شانه قرش')
     design = models.ForeignKey(to='Design', on_delete=models.CASCADE, related_name='نقشه', verbose_name='نقشه فرش')
+    price = models.IntegerField(verbose_name='قیمت', default=0)
 
     def __str__(self):
         return self.title
@@ -23,6 +24,7 @@ class Size(models.Model):
 class Material(models.Model):
     title = models.CharField(max_length=64, verbose_name='جنس')
     image = models.ImageField(upload_to='materials', verbose_name="عکس")
+    price = models.IntegerField(verbose_name='قیمت')
 
     def __str__(self):
         return self.title
