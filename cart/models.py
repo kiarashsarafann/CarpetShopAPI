ba6ecf1929f601d97aba3956b4f2dc43559243bf
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2024-08-30 08:16:49 +0330

    make cart models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from carpets.models import Material, Carpet
+from users.models import CustomUser
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    carpet = models.ForeignKey(Carpet, on_delete=models.CASCADE, null=True, blank=True)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.IntegerField(default=0)
+
+    def get_total_price(self):
+        if self.carpet is not None:
+            return self.quantity * self.carpet.price
+        elif self.material is not None:
+            return self.quantity * self.material.price
